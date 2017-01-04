c653a661c6a706a5ecc21e0e77f4c8378da11802
syw <mooniscrazy@hotmail.com>
2017-01-04 20:45:46 +0800

    a

---
+++ ListImpl_Add/ListImpl_Add/MyList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListImpl_Add {
+    class MyList {
+        private int[] innerArray = new int[4];//数组初始大小为4
+        public void Add(int value) {
+            throw new NotImplementedException();
+        }
+
+        public int this[int index] {
+            get {
+                throw new NotImplementedException();
+            }
+            set {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ ListImpl_Add/ListImpl_Add/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListImpl_Add {
+    class Program {
+        static void Main(string[] args) {
+            MyList list = new MyList();
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            Console.WriteLine(list[0]);
+            Console.WriteLine(list[1]);
+            Console.WriteLine(list[2]);
+        }
+    }
+}
